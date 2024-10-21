7527fbb1ea77dee5e4507cfa804487beadf03d64
Bilalahmad02 <bilal.ahma10@gmail.com>
2024-10-21 22:14:43 +0200

    Create User  model

--- Kanban Server/models/User.cs
+++ Kanban Server/models/User.cs
@@ -1,12 +1,9 @@
-namespace KanbanServer.Models
-{
-    public class User
-{
-    public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public byte[] PasswordHash { get; set; } = new byte[32];
-    public byte[] PasswordSalt { get; set; } = new byte[32];
-    public List<Board> Boards { get; set; } = new();
-}
+using Microsoft.AspNetCore.Identity;
 
+namespace Kanban_Server.Models
+{
+    public class User : IdentityUser
+    {
+        public List<Board> Boards { get; set; } = new();
+    }
 }
