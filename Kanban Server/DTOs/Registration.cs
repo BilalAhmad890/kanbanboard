35f71791de050138bc0e74ddf9d9f75d105bcf18
Bilalahmad02 <bilal.ahma10@gmail.com>
2024-10-23 13:29:54 +0200

    Create RegisterDto for user registration

---
+++ Kanban Server/DTOs/Registration.cs
@@ -0,0 +1,9 @@
+namespace Kanban_Server.DTOs
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
