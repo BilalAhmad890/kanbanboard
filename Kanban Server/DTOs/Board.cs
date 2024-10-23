f9dc0e6a22ad347cf13d7f9a4b915dd88d641bc0
Bilalahmad02 <bilal.ahma10@gmail.com>
2024-10-23 13:27:20 +0200

    Create BoardDto with validation attributes

---
+++ Kanban Server/DTOs/Board.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Kanban_Server.DTOs
+{
+     public class BoardDto
+    {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Title must be between 1 and 100 characters")]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(500, MinimumLength = 1, ErrorMessage = "Description must be between 1 and 500 characters")]
+        public string Description { get; set; } = string.Empty;
+    }
+
+}
