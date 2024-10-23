2253fcc36f62e44e83ff33b4805726edfecfefc1
Bilalahmad02 <bilal.ahma10@gmail.com>
2024-10-23 13:25:25 +0200

    Replaced BulletPoints with LinkUrl and LinkName in Task model

--- Kanban Server/models/Task.cs
+++ Kanban Server/models/Task.cs
@@ -1,17 +1,16 @@
-using Microsoft.VisualBasic;
 
 namespace Kanban_Server.Models
 {
     public class Task
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string BulletPoints { get; set; } = string.Empty;
-    public DateTime DateAndTime { get; set; } = DateTime.Now;
-    public TaskStatus Status { get; set; } = TaskStatus.Backlog;  // Default status is "Backlog"
-    public int BoardId { get; set; }
-    public Board Board { get; set; } = null!;
-}
-
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string LinkUrl { get; set; } = string.Empty;    // The actual URL of the link
+        public string LinkName { get; set; } = string.Empty;   // The name/title of the link
+        public DateTime DateAndTime { get; set; } = DateTime.Now;
+        public TaskStatus Status { get; set; } = TaskStatus.Backlog;  // Default status is "Backlog"
+        public int BoardId { get; set; }
+        public Board Board { get; set; } = null!;
+    }
 }
