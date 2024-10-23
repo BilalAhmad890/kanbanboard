224824d33eee03dbd8aab84d44da4109b84c1aaf
Bilalahmad02 <bilal.ahma10@gmail.com>
2024-10-23 13:27:57 +0200

    Added GetBoardWithTasksDto for retrieving board data with tasks

---
+++ Kanban Server/DTOs/GetBoard.cs
@@ -0,0 +1,10 @@
+namespace Kanban_Server.DTOs
+{
+    public class GetBoardWithTasksDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<GetTaskDto> Tasks { get; set; } = new List<GetTaskDto>();
+    }
+}
