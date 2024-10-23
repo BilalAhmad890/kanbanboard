dd7b12f1dbe097bfb1b0157051a85e5cde3b4ef6
Bilalahmad02 <bilal.ahma10@gmail.com>
2024-10-23 13:24:10 +0200

    Add Description property to Board model

--- Kanban Server/models/Board.cs
+++ Kanban Server/models/Board.cs
@@ -5,9 +5,9 @@ public class Board
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public List<Task> Tasks { get; set; } = new();
     public string UserId { get; set; } = string.Empty;
-
     public User User { get; set; } = null!;
 
     }
