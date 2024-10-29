3acf4b7d59cb9e28522d5860ea35310c58a315fb
Bilalahmad02 <bilal.ahma10@gmail.com>
2024-10-29 21:28:25 +0100

    Updated Getboard.cs and added profile.cs file

--- Kanban Server/DTOs/GetBoard.cs
+++ Kanban Server/DTOs/GetBoard.cs
@@ -5,6 +5,7 @@ namespace Kanban_Server.DTOs
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public DateTime? Created { get; set; }
         public List<GetTaskDto> Tasks { get; set; } = new List<GetTaskDto>();
     }
 }
---
+++ Kanban Server/DTOs/ProfileDto.cs
@@ -0,0 +1,8 @@
+using Kanban_Server.Models;
+
+namespace Kanban_Server.DTOs {
+    public class ProfileDto
+{
+    public string UserName { get; set; } 
+}
+}
